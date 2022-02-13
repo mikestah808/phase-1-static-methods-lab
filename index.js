0967b7794c7db9d7c4299b3b2d7fc01ad48c4924
Michael Stafford <michaelstafford808@gmail.com>
2022-02-13 10:50:06 -1000

    commit tree a537be45e03dba177aa05b921825f37beab2db6f

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^-,'A-Za-z0-9 ]+/g, '')
+  }
+
+  static titleize(sentence){
+    const arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    let modSentence = sentence.split(" ").map(s => {
+      if(!arr.includes(s)){
+
+      return Formatter.capitalize(s)
+    } else {
+      return s;
+    }
+
+  }).join(" ")
+
+  return Formatter.capitalize(modSentence)
+
+  }
+
 }
